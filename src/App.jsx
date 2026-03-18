b824dbe9fc0cd3b3db15c5da89192cfc67d49e1f
Harsh-Shrivastava1 <hshrivastava23032007@gmail.com>
2026-03-18 20:08:03 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,16 @@ import { BookingProvider } from "./context/BookingContext";
 import AppRouter from "./routes/AppRouter";
 import "./styles/global.css";
 import "./styles/animation.css";
-import { useState } from "react";
-import LaunchScreen from "./components/LaunchScreen";
-import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
-    const [loading, setLoading] = useState(true);
-
     return (
-        <>
-            <AnimatePresence mode="wait">
-                {loading && <LaunchScreen onComplete={() => setLoading(false)} />}
-            </AnimatePresence>
-
-            {!loading && (
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 1 }}
-                >
-                    <BrowserRouter>
-                        <AuthProvider>
-                            <BookingProvider>
-                                <AppRouter />
-                            </BookingProvider>
-                        </AuthProvider>
-                    </BrowserRouter>
-                </motion.div>
-            )}
-        </>
+        <BrowserRouter>
+            <AuthProvider>
+                <BookingProvider>
+                    <AppRouter />
+                </BookingProvider>
+            </AuthProvider>
+        </BrowserRouter>
     );
 }
 
